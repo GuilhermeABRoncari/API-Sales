ac26dcf677cb6417d6f273afaabad9c1489c792f
Guilherme Alves Batista Roncari <123610693+GuilhermeABRoncari@users.noreply.github.com>
2023-02-07 21:20:07 -0300

    Alterações para testar um commit

--- HelloWorldJava/src/Main.java
+++ HelloWorldJava/src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //fazendo algumas alterações para testar os commits
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n1 = teclado.nextInt();
+        System.out.println("Enter another number");
+        int n2 = teclado.nextInt();
+        int soma = n1 + n2;
+        System.out.println("sum of " + n1 + " and " + n2 + " = " + soma);
     }
 }
