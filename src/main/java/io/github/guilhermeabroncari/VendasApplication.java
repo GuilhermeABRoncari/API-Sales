3aec9da86343ea06af5ce2c15c74c966897690fb
Guilherme Alves Batista Roncari <123610693+GuilhermeABRoncari@users.noreply.github.com>
2023-02-20 16:15:44 -0300

    test

--- src/main/java/io/github/guilhermeabroncari/VendasApplication.java
+++ src/main/java/io/github/guilhermeabroncari/VendasApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VendasApplication {
+    String test = "alterations";
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
